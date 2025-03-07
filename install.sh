5e994f538e1013e74a60ae712bf30e7a305349fd
Nurindra <indraagustian1@gmail.com>
2025-03-07 22:17:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,32 +20,8 @@ sudo apt install -y php8.1 php8.1-cli php8.1-curl php8.1-mbstring php8.1-xml php
 sudo a2enmod php8.1
 sudo systemctl restart apache2
 
-# Install Composer
-COMPOSER_INSTALLER="https://getcomposer.org/installer"
-INSTALLER_HASH="$(curl -s https://composer.github.io/installer.sig)"
-php -r "copy('$COMPOSER_INSTALLER', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '$INSTALLER_HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); exit(1); } echo PHP_EOL;"
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-php -r "unlink('composer-setup.php');"
-
-# Tambahkan composer ke user biasa
-USERNAME="${SUDO_USER:-$(whoami)}"
-USER_HOME="$(eval echo ~$USERNAME)"
-mkdir -p "$USER_HOME/.local/bin"
-composer config --global bin-dir "$USER_HOME/.local/bin"
-
-# Tambahkan PATH user biasa
-if ! grep -q "export PATH=\$PATH:\$HOME/.local/bin" "$USER_HOME/.bashrc"; then
-  echo "export PATH=\$PATH:\$HOME/.local/bin" >> "$USER_HOME/.bashrc"
-fi
-
-# Restart shell agar konfigurasi PATH berlaku
-sudo chown -R $USERNAME:$USERNAME "$USER_HOME/.local"
-source "$USER_HOME/.bashrc"
-
 # Verifikasi Instalasi
 apache2 -v
 php -v
-composer --version
 
 echo "Instalasi selesai!"
